fe5b22fd6e4d795eea0017028570000119e32ee7
ParthMehta15 <46846270+ParthMehta15@users.noreply.github.com>
2022-04-25 18:10:36 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,25 +5,81 @@ import pandas as pd
 import streamlit as st
 
 
+prob_storm = 0.5
+sensitivity = 0.799
+specificity = 0.559
 
-with st.echo(code_location='below'):
-    chance_of_mold = st.slider("Chance of botrytis ", 1, 100, 1)
-    no_sugar = st.slider("Chance of no sugar", 1, 100, 1)
-    no_typical = st.slider("Chance of typical sugar", 1, 100 - no_sugar, 1)
+#if harvest_now:
+exp_payout_harvest_now = 12 * (6000*5 + 2000*10 + 2000*15)  #960,000
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+#if storm
+exp_payout_no_mold = 12*(5000*5 + 1000*10)  #420,000
+exp_payout_mold = 12*(5000*5 + 1000*10 + 2000*120)  #3,300,000
 
-    points_per_turn = total_points / num_turns
+#if no storm
+exp_payout_no_sugar = 12 * (6000*5 + 2000*10 + 2000*15)   #960,000
+exp_payout_typical_sugar = 12 * (5000*5 + 1000*10 + 2500*15 + 1500*30)    #1,410,000
+exp_payout_high_sugar = 12 * (4000*5 + 2500*10 + 2000*15 + 1000*30 + 500*40)  #1,500,000
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+#Specificity-> True negative rate (Specificity = (True Negative)/(True Negative + False Positive)) -> TN -> D No rain
+#Sensitivity-> True positivetive rate (Sensitivity = (True Positive)/(True Positive + False Negative) -> TP -> D Rain
+
+def predict(prob_storm, sensitivity, specificity, payout_harvest, payout_no_harvest_storm, payout_no_harvest_no_storm):
+  p_dns = specificity * (1-prob_storm) + (1-sensitivity) * prob_storm
+  p_ns_dns = specificity * (1-prob_storm) / p_dns
+  path = ''
+  # print('P(DNS): ',p_dns)
+  # print('P(NS|DNS): ',p_ns_dns)
+
+  p_ds = sensitivity * prob_storm + (1-specificity) * (1 - prob_storm) # (1-p_dns)
+  p_s_ds = sensitivity * prob_storm / p_ds
+  # print('P(DS): ',p_ds)
+  # print('P(S|DS): ',p_s_ds)
+
+  exp_ds = max(payout_harvest, payout_no_harvest_no_storm*(1-p_s_ds) + payout_no_harvest_storm * p_s_ds)
+  exp_nds = max(payout_harvest, payout_no_harvest_no_storm*p_ns_dns + payout_no_harvest_storm * (1-p_ns_dns))
+
+  expected_payout_detector = p_dns * exp_nds + p_ds * exp_ds
+
+  final_expected_payout = max(expected_payout_detector,payout_harvest)
+  if payout_harvest >= expected_payout_detector:
+      path = 'HARVEST NOW'
+  else:
+      path = 'WAIT TO HARVEST'
+
+  # print('Expected Value: ',expected_payout)
+  return final_expected_payout, path
+
+
+# with st.echo(code_location='below'):
+
+st.header('STORM PREDICTOR')
+
+chance_of_mold = st.slider(label = "Chance of botrytis ", min_value = 0, max_value = 100, step = 1)
+no_sugar = st.slider(label = "Chance of no sugar", min_value = 0, max_value = 100, step = 1)
+high_sugar = 0
+typical_sugar = 0
+if no_sugar != 100:
+    typical_sugar = st.slider(label = "Chance of typical sugar",  min_value = 0, max_value = 100 - no_sugar, step = 1)
+if no_sugar+typical_sugar!=100:
+    high_sugar = st.slider(label = "Chance of high sugar",  min_value = 0, max_value = 100 - no_sugar - typical_sugar , step = 1)
+
+st.text('Chance of botrytis mold: ' +str(chance_of_mold)+'%')
+st.text('Chance of no sugar: ' + str(no_sugar)+'%')
+st.text('Chance of typical sugar: ' + str(typical_sugar)+'%')
+st.text('Chance of high sugar: ' + str(high_sugar)+'%')
+
+if no_sugar+typical_sugar+high_sugar ==100:
+
+    #total of all sugar = 100
+    expected_payout_wo_storm = (exp_payout_no_sugar*no_sugar + exp_payout_typical_sugar*typical_sugar + exp_payout_high_sugar*high_sugar)/100
+    expected_payout_with_storm = (exp_payout_mold*chance_of_mold + exp_payout_no_mold*(100-chance_of_mold))/100
+
+    final_expected_value, decision = predict(prob_storm, sensitivity, specificity, exp_payout_harvest_now, expected_payout_with_storm, expected_payout_wo_storm)
+
+
+    st.subheader('FINAL EXPECTED PAYOUT VALUE: $'+str(final_expected_value))
+    st.subheader('DECISION: '+str(decision))
+else:
+    st.subheader('Chances of sugar should sum to 100')
